50316f65be04140e84832e2eccac0ee106ece385
Sabir164 <sabirkg@code.edu.az>
2023-10-28 03:10:21 -0700

    homework28

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,145 @@
+﻿#region task1
+//Console.WriteLine("4 reqemli eded daxil edin..");
+//int num = int.Parse(Console.ReadLine());
+//string tvin = num.ToString();
+//while (true)
+//{
+//    if (tvin.Length == 4)
+//    {
+//        Console.WriteLine("7" + tvin + "8");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        tvin = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task2
+//Console.WriteLine("Ededi daxil edin:");
+//int numt = int.Parse(Console.ReadLine());
+//string git = numt.ToString();
+//while (true)
+//{
+
+//    if (git.Length == 3)
+//    {
+//        Console.WriteLine(git + git);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        git = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task3
+//Console.WriteLine("Ededi daxil edin..");
+//int num3 = int.Parse(Console.ReadLine());
+//string git1 = num3.ToString();
+//while (true)
+//{
+//    if (git1.Length == 5)
+//    {
+//        double result5 = num3 * 0.18;
+//        double result6 = result5 * 0.3;
+//        Console.WriteLine(result6);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        git1 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task4
+//Console.WriteLine("3 reqemli eded daxil edin");
+//int num4 = int.Parse(Console.ReadLine());
+//string git2 = num4.ToString();
+//while (true)
+//{
+//    if (git2.Length == 3)
+//    {
+//        int numstring1 = num4 * 10 + 7;
+//        int numstring2 = (numstring1 * 7) / 100;
+
+//        Console.WriteLine("Net: " + numstring2);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        git2 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task5
+//Console.WriteLine("4 reqemli eded daxil edin");
+//int num5 = int.Parse(Console.ReadLine());
+//string git3 = num5.ToString();
+//while (true)
+//{
+//    if (git3.Length == 4)
+//    {
+//        string result2 = "4" + git3 + "44";
+//        int resultInt = int.Parse(result2);
+//        double lastResult = resultInt * 0.44;
+//        Console.WriteLine(lastResult);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        git3 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task6
+//Console.WriteLine("4 reqemli eded daxil edin");
+//int num6 = int.Parse(Console.ReadLine());
+//string git4 = num6.ToString();
+//while (true)
+//{
+//    if (git4.Length == 4)
+//    {
+//        double firstsolution = num6 * 0.20;
+//        double Secondsolution = firstsolution * 0.10;
+//        double result = Secondsolution * Secondsolution;
+//        Console.WriteLine(result);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        git4 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task7
+//int num7 = int.Parse(Console.ReadLine());
+//int num8 = int.Parse(Console.ReadLine());
+//string git6 = num8.ToString();
+//string git7 = num7.ToString();
+//while (true)
+//{
+//    if (git6.Length == 5 && git7.Length == 5)
+//    {
+//        int firsttest = num7 + num8;
+//        string firstsolutiont = firsttest.ToString();
+//        string resultT = "5" + firsttest + "5";
+//        int resultsolution2 = int.Parse(resultT);
+//        double endResult = resultsolution2 * 0.05;
+//        Console.WriteLine(endResult);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        git7 = Console.ReadLine();
+//        git6 = Console.ReadLine();
+//    }
+//}
+#endregion
